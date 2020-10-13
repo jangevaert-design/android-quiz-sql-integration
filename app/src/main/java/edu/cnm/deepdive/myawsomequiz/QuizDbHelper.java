3b3301c1db0be9290762383370c4f1a5cf726872
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-12 19:00:06 -0600

    Turned QiozDbHelper into a singleton to avoid memory leaks when accessing from different activities.

--- app/src/main/java/edu/cnm/deepdive/myawsomequiz/QuizDbHelper.java
+++ app/src/main/java/edu/cnm/deepdive/myawsomequiz/QuizDbHelper.java
@@ -17,10 +17,19 @@ public class QuizDbHelper extends SQLiteOpenHelper {
   private static final String DATABASE_NAME = "MyAwesomeQuiz.db";
   private static final int DATABASE_VERSION = 1;
 
+  private static QuizDbHelper instance;
+
   private SQLiteDatabase db;
 
-  public QuizDbHelper(Context context) {
+  private QuizDbHelper(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
+  }
+
+  public static synchronized QuizDbHelper getInstance(Context context) {
+    if (instance == null) {
+      instance = new QuizDbHelper(context.getApplicationContext());
+    }
+    return instance;
   }
 
   @Override
@@ -126,6 +135,24 @@ public class QuizDbHelper extends SQLiteOpenHelper {
 
   }
 
+  public List<Category> getAllCategories() {
+    List<Category> categoryList = new ArrayList<>();
+    db= getReadableDatabase();
+    Cursor c = db.rawQuery("SELECT * FROM " + CategoriesTable.TABLE_NAME, null);
+
+    if (c.moveToFirst()) {
+      do {
+        Category category = new Category();
+        category.setId(c.getInt(c.getColumnIndex(CategoriesTable._ID)));
+        category.setName(c.getString(c.getColumnIndex(CategoriesTable.COLUMN_NAME)));
+        categoryList.add(category);
+      } while (c.moveToNext());
+    }
+
+    c.close();
+    return categoryList;
+  }
+
   public ArrayList<Question> getAllQuestions() {
     ArrayList<Question> questionList = new ArrayList<>();
     db = getReadableDatabase();
@@ -151,13 +178,23 @@ public class QuizDbHelper extends SQLiteOpenHelper {
     return questionList;
   }
 
-  public ArrayList<Question> getQuestions(String difficulty) {
+  public ArrayList<Question> getQuestions(int categoryId, String difficulty) {
     ArrayList<Question> questionList = new ArrayList<>();
     db = getReadableDatabase();
 
-    String[] selectionArgs = new String[] {difficulty};
-    Cursor c = db.rawQuery("SELECT * FROM " + QuestionsTable.TABLE_NAME +
-        " WHERE " + QuestionsTable.COLUMN_DIFFICULTY + " = ?", selectionArgs);
+    String selection = QuestionsTable.COLUMN_CATEGORY_ID + " = ?" +
+        " AND " + QuestionsTable.COLUMN_DIFFICULTY + " = ?";
+    String[] selectionArgs = new String[] {String.valueOf(categoryId), difficulty};
+
+    Cursor c = db.query(
+        QuestionsTable.TABLE_NAME,
+        null,
+        selection,
+        selectionArgs,
+        null,
+        null,
+        null
+    );
 
     if (c.moveToFirst()) {
       do {
